1a6d674fd94d850b9207c43bad3d436789e63108
Peter Wirdemo <peter.wirdemo@gmail.com>
2020-04-06 18:54:33 +0200

    removed username recon

--- docker/webcmd/dnscrawler.pl
+++ docker/webcmd/dnscrawler.pl
@@ -7,6 +7,7 @@ use NetAddr::IP;
 use Socket;
 use File::Copy;
 
+
 my($debug) = undef;
 
 sub read_db($) {
@@ -65,14 +66,21 @@ my($cluster) = undef,
 my($command) = undef;
 my($force) = undef;
 my($db) = $0 . ".db";
+my($ldebug) = undef;
 GetOptions (
          "network=s" => \$network,
          "cluster=s" => \$cluster,
-         "debug:i"  => \$debug,
+         "debug:i"  => \$ldebug,
          "command=s" => \$command,
          "force" => \$force,
 ) or die("Error in command line arguments\n");
 
+if ( defined($ldebug)) {
+	if ( $ldebug < 1 ) {
+		$ldebug = 1;
+	}
+	$debug=$ldebug;
+}
 
 my($usage) = "Usage: $0 --network=<network> --cluster=<cluster tag> --command=<command to execute> --force --debug\n";
 die "Missing network, " . $usage unless ( $network );
@@ -83,30 +91,29 @@ my $ip = NetAddr::IP->new($network);
 die "Cant create NetAddr::IP object from $network, " . $usage unless ( $ip );
 
 my(%db) = read_db($db);
-print Dumper(\%db) if ( $debug );
+#print Dumper(\%db) if ( $debug );
 
 my($addr);
 foreach $addr( $ip->hostenum() ) {
+	my($client) = $addr->addr();
    my $iaddr = $addr->aton();
-	my $name  = gethostbyaddr($iaddr, AF_INET);
+	my $host  = gethostbyaddr($iaddr, AF_INET);
+	next unless ( defined($host) );
+   next unless ( $host =~ /(\w+)-$cluster/ );
+	my($name) = $1;
 	next unless ( defined($name) );
-   next unless ( $name =~ /(\w+)-$cluster/ );
-	my($user) = $1;
-	next unless ( defined($user) );
-   my $uid = getpwnam($user);
-   next unless ( defined($uid) );
-	print "ip: $addr name: $name ($user)\n" if ( $debug );
+	print "ip: $client host: $host ($name)\n" if ( $debug );
 
-	my($cmd) = $command . " --ip=$addr --name=$name";
+	my($cmd) = $command . " --ip=$client --host=$host";
 
-	my($time) = $db{$addr}{$command};
+	my($time) = $db{$client}{$command};
 	if ( defined($time) and !defined($force) ) {
-		print "Error: '$command' was executed on '$addr' on " . localtime($time) . " ($time)\n";
+		print "Error: '$command' was executed on '$client' on " . localtime($time) . " ($time)\n";
 		print "use the force to force the re-execution of this command ( --force )\n";
 		next;
 	}
 	else {
-		$db{$addr}{$command}=time;
+		$db{$client}{$command}=time;
 		write_db($db,%db);
 
 		print $cmd . "\n";
